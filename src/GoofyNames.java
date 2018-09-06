bee7dd74247ae9e95c84ddde2bfd38bb0385edbf
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-09-06 13:31:51 -0700

    goofy names finished guessing pi not finished

--- src/GoofyNames.java
+++ src/GoofyNames.java
@@ -18,20 +18,25 @@ public class GoofyNames {
 		//    HINT: Use .length() to determine the number of characters in the String.
 		for (int i = 0; i < name.length(); i++) {
 			
-		}
+		
 			// 4. Create a String variable to store the next character of the name
 			//    using .substring(start, end)
 			//    HINT: replace 'start' and 'end' to get String with character at i 
 			String nextC = name.substring(i,i+1);
 			// 5. Use MODULO to set this variable to upper case for EVEN characters
 			//    and lower case for ODD characters.
-
-			
+			if(i%2 == 0) {
+			nextC = nextC.toUpperCase();
+			}
+			else {
+			nextC = nextC.toLowerCase();
+			}
 			// 6. ADD this String  (containing 1 char) to the goofyName String
-
+			goofyName = goofyName + nextC;
 	}
 		// 7. Use pop-up to show user their Goofy name
-
+		JOptionPane.showMessageDialog(null,goofyName);
 	}
 }
+
 
--- src/GuessingPi.java
+++ src/GuessingPi.java
@@ -7,21 +7,25 @@ import javax.swing.JOptionPane;
 
 public class GuessingPi {
 	// 1. Make a main method and make sure your program can run
+public static void main(String[] args) {
+	
 
 		// 2. Make a String variable to hold the value of Pi.
 		//    Get the first few digits from http://www.piday.org/million/.
-
+		String pi = "3.1415926535";
 		// 3. Print out the first 3 digits of Pi to the console.
 		//    The first value is "pi.charAt(0)", the second is "pi.charAt(1)", etc.
 		//    Run your program to see how this works.
-
+														
 		// 9. If you want to give the user more than one chance to guess,
 		//    put a for loop around steps 4-8.
-
+		for (int i = 0; i < pi.length(); i++) {
+			System.out.print("" + pi.charAt(0));
+		
 			// 4. Create a for loop that will step through each digit of pi (steps 5-8)
 			//    NOTE: The number of digits in your string will be
 			//       yourStringVariable.length()
-
+			
 				// 5. Ask the user for the NEXT digit of pi.
 
 				// 6. Compare the user's input to the next digit of your pi variable 
@@ -32,7 +36,7 @@ public class GuessingPi {
 
 					// 8. If they are not, print out "incorrect" and tell them
 					//    to start over. Use 'break;' to break out of the loop.
-
+		}
 }
-
+}
 
