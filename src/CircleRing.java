4f601244d6757f7a7b5982b5ab77392d8acd27fd
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-09-13 13:23:22 -0700

    Finished both circlering and guessing pi

--- src/CircleRing.java
+++ src/CircleRing.java
@@ -1,3 +1,4 @@
+
 /*
  *    Copyright (c) The League of Amazing Programmers 2013-2017
  *    Level 0
@@ -6,29 +7,36 @@
 import org.jointheleague.graphical.robot.Robot;
 
 public class CircleRing {
-    public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws Exception {
 
-        // 1. Make a new Robot
+		// 1. Make a new Robot
+		Robot bender = new Robot();
+		// 2. Set your robot’s position to x=150 and y=200
+		bender.setX(150);
+		bender.setX(200);
+		// 3. Put the robot's pen down
+		bender.penDown();
+		// 4. Set the robot’s speed to 10
+		bender.setSpeed(100);
+		// 5. Do everything below here 360 times (use i as the counter)
+		for (int i = 0; i < 360; i++) {
 
+			// 6. Move the robot 3 pixels
+			bender.move(3);
+			// 7. Turn the robot 1 degree
+			bender.turn(1);
+			// 8. If the counter i is divisible by 20 (hint: use mod operator %)..
+			if (i % 20 == 0) {
 
-       //  2. Set your robot’s position to x=150 and y=200
+				// 9. do steps 10 and 11 exactly 360 times (use j as the counter)
+				for (int j = 0; j < 360; j++) {
 
-        // 3. Put the robot's pen down
-
-        // 4. Set the robot’s speed to 10
-
-        // 5. Do everything below here 360 times (use i as the counter)
-
-	        // 6. Move the robot 3 pixels
-
-       //  7. Turn the robot 1 degree
-
-        // 8. If the counter i is divisible by 20 (hint: use mod operator  %)..
-
-		// 9. do steps 10 and 11 exactly 360 times (use j as the counter)
-
-				// 10. Move the robot 1 pixel
-
-				// 11. Turn the robot 1 degree
-}
+					// 10. Move the robot 1 pixel
+					bender.move(1);
+					// 11. Turn the robot 1 degree
+					bender.turn(1);
+				}
+			}
+		}
+	}
 }
