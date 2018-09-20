7878c9e99e9f4edf33728decaf3b53062a301236
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-09-20 13:22:58 -0700

    almost finished thanosgauntlet

---
+++ src/ForLoopThanosGauntlet.java
@@ -0,0 +1,26 @@
+import javax.swing.JOptionPane;
+
+public class ForLoopThanosGauntlet {
+public static void main(String[] args) {
+	
+	for (int i = 0; i < 101; i++) {
+	System.out.println(i);
+	}
+	
+	for (int i = 100; i > -1; i--) {
+	System.out.println(i);
+	}
+	
+	for (int i = 2; i < 101; i+=2) {
+	System.out.println(i);
+	}
+	
+	for (int i = 1; i < 100; i+=2) {
+	System.out.println(i);
+	}
+	
+	
+	
+	
+}
+}
