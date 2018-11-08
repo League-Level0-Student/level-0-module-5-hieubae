473969b0739ef6ae4df294ae4c1f8d7fefe23bf6
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-11-08 13:24:43 -0800

    Finished Skill Pratice Worksheet

--- src/extra/SkillPraticeWorksheet.java
+++ src/extra/SkillPraticeWorksheet.java
@@ -69,26 +69,37 @@ public class SkillPraticeWorksheet {
 	}
 
 	void skill4() { // In a pop-up, ask the user for the city they live in
-
+		String city = JOptionPane.showInputDialog(null, "What city do you live in?");
 		// If they answered "San Diego", tell them they live in America's Finest City
-
+		if (city.equals("San Diego")) {
+		JOptionPane.showMessageDialog(null, "You live in America's finest city.");
+		}
 		// Otherwise, tell them to move to San Diego
-
+		else {
+		JOptionPane.showMessageDialog(null, "Move to San Diego.");
+		}
 		// Create a variable - cars - and initialize it to the number of cars your
 		// family has. // If there are 0 cars, use a pop-up to display, "I bet you use
 		// public transportation."
-
+		int cars = 2;
+		if (cars == 0) {
+		JOptionPane.showMessageDialog(null, "I bet you use public transportation");
+		}
 		// If there is 1 car, use a pop-up to display the make/model of the car
-
+		else if (cars == 1) {
+		JOptionPane.showMessageDialog(null, "You have a lambo.");
+		}
 		// If there is more than 1 car, use a pop-up to display how many wheels the //
 		// cars have between them.
-
+		else if (cars > 2) {
+		JOptionPane.showMessageDialog(null, "You have 8 wheels");
+		}
 	}
 
 	void skill5() { // In a pop-up, ask the user for the name of their school
-
+	String school =	JOptionPane.showInputDialog(null, "What's the name of your school?");
 		// In another pop-up, tell the user, that their school is a fantastic school. //
 		// You must include the name of the school in the message.
-
+	JOptionPane.showMessageDialog(null, school + " is a fantastic school.");
 	}
 }
